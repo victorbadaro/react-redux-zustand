b3edbd8ba4569f31c926034a3ae0fb310eadb3d3
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-10-09 12:59:22 -0300

    feat: add a scrollbar in the sidebar

--- eslint.config.js
+++ eslint.config.js
@@ -26,7 +26,8 @@ export default tseslint.config(
 			quotes: ['error', 'single'],
 			'jsx-quotes': ['error', 'prefer-double'],
 			semi: ['error', 'always'],
-			'comma-dangle': ['error', 'never']
+			'comma-dangle': ['error', 'never'],
+			'@typescript-eslint/no-require-imports': 'off'
 		}
 	}
 );
