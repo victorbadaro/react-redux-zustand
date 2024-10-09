5adc0d1ff9c55223b237f5fffe4a66cedd19e004
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-10-09 16:17:07 -0300

    feat: add collapsible component in the sidebar

--- src/components/module.tsx
+++ src/components/module.tsx
@@ -1,3 +1,4 @@
+import * as Collapsible from '@radix-ui/react-collapsible';
 import { ChevronDown } from 'lucide-react';
 import { Lesson } from './lesson';
 
@@ -9,21 +10,23 @@ interface Module {
 
 export function Module({ moduleIndex, title, amountOfLessons }: Module) {
 	return (
-		<div>
-			<button type="button" className="flex w-full items-center gap-3 bg-zinc-800 p-4">
+		<Collapsible.Root className="group">
+			<Collapsible.Trigger type="button" className="flex w-full items-center gap-3 bg-zinc-800 p-4">
 				<div className="flex size-10 rounded-full items-center justify-center bg-zinc-950 text-xs">{moduleIndex + 1}</div>
 				<div className="flex flex-col gap-1 text-left">
 					<strong className="text-sm">{title}</strong>
 					<span className="text-xs text-zinc-400">{amountOfLessons} aulas</span>
 				</div>
-				<ChevronDown className="size-5 ml-auto text-zinc-400" />
-			</button>
+				<ChevronDown className="size-5 ml-auto text-zinc-400 group-data-[state=open]:rotate-180 transition-transform" />
+			</Collapsible.Trigger>
 
-			<nav className="relative flex flex-col gap-4 p-6">
-				<Lesson title="Fundamentos do Redux" duration="09:13" />
-				<Lesson title="Fundamentos do Redux" duration="09:13" />
-				<Lesson title="Fundamentos do Redux" duration="09:13" />
-			</nav>
-		</div>
+			<Collapsible.Content>
+				<nav className="relative flex flex-col gap-4 p-6">
+					<Lesson title="Fundamentos do Redux" duration="09:13" />
+					<Lesson title="Fundamentos do Redux" duration="09:13" />
+					<Lesson title="Fundamentos do Redux" duration="09:13" />
+				</nav>
+			</Collapsible.Content>
+		</Collapsible.Root>
 	);
 }
