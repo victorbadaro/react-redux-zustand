d52a6fe091916b72df2f4c2c376a8f25296da247
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-10-10 13:04:54 -0300

    feat: add autoplay

--- src/components/module.tsx
+++ src/components/module.tsx
@@ -21,7 +21,7 @@ export function Module({ moduleIndex, title, amountOfLessons }: Module) {
 	const lessons = useAppSelector(state => state.player.course.modules[moduleIndex].lessons);
 
 	return (
-		<Collapsible.Root className="group">
+		<Collapsible.Root className="group" defaultOpen={moduleIndex === 0}>
 			<Collapsible.Trigger type="button" className="flex w-full items-center gap-3 bg-zinc-800 p-4">
 				<div className="flex size-10 rounded-full items-center justify-center bg-zinc-950 text-xs">{moduleIndex + 1}</div>
 				<div className="flex flex-col gap-1 text-left">
--- src/components/video-player.tsx
+++ src/components/video-player.tsx
@@ -1,7 +1,10 @@
 import { useAppSelector } from '@/store';
+import { next } from '@/store/slices/player';
 import ReactPlayer from 'react-player';
+import { useDispatch } from 'react-redux';
 
 export function VideoPlayer() {
+	const dispatch = useDispatch();
 	const lesson = useAppSelector(state => {
 		const { currentModuleIndex, currentLessonIndex } = state.player;
 		const currentLesson = state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex];
@@ -9,12 +12,18 @@ export function VideoPlayer() {
 		return currentLesson;
 	});
 
+	function handlePlayNext() {
+		dispatch(next());
+	}
+
 	return (
 		<div className="w-full bg-zinc-950 aspect-video">
 			<ReactPlayer
 				width="100%"
 				height="100%"
 				controls
+				playing
+				onEnded={handlePlayNext}
 				url={`https://www.youtube.com/watch?v=${lesson.id}`}
 			/>
 		</div>
